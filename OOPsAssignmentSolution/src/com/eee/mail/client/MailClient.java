9b25258f4345e0072f98078f45de9ee69729d463
LAPTOP-3BBK4LB9\LEKSHITH & JENVITH <venkatit1215@gmail.com>
2022-03-27 09:37:57 +0530

    New project OOPsAssignmentSolution created

---
+++ OOPsAssignmentSolution/src/com/eee/mail/client/MailClient.java
@@ -0,0 +1,18 @@
+package com.eee.mail.client;
+
+import java.util.Scanner;
+
+public class MailClient {
+
+	public static void main(String[] args) {
+		Scanner scanner =  new Scanner(System.in);
+		
+		
+
+		scanner.close();
+		
+		
+
+	}
+
+}
---
+++ OOPsAssignmentSolution/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author LEKSHITH & JENVITH
+ *
+ */
+module OOPsAssignmentSolution {
+}
